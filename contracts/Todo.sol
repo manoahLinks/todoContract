719da97e90e9afb170df479161b6e5e1e2b89943
manoahLinks <manoahluka@gmail.com>
2024-02-13 03:57:39 +0100

    initila commit

---
+++ contracts/Todo.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.2;
+
+contract TodoContract {
+
+    // todo struct
+    struct Todo {
+        string title;
+        string description;
+        bool isDone;
+    }
+
+   
+
+}
